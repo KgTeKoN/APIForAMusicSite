5086f314bcbd824500f60e782f0af2109d91aa97
Андрій Петруняк <hasherama8@gmail.com>
2022-03-16 16:44:40 +0200

    delete user

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { userRouter } from './routes/Users/userCreateRoute';
 import { userGetOneRoute } from './routes/Users/userGetOneRoute';
 import { userAuthorsGetRoute } from './routes/Users/userAuthorsGetRoute';
+import { userDeleteRoute } from './routes/Users/userDeleteRoute';
 
 const app = new App();
 
@@ -10,6 +11,7 @@ app.app.use(express.json());
 app.app.use('/user', userRouter);
 app.app.use('/user', userGetOneRoute);
 app.app.use('/user', userAuthorsGetRoute);
+app.app.use('/user', userDeleteRoute);
 
 async function bootstrap(): Promise<void> {
 	await app.init();
---
+++ src/routes/Users/userDeleteRoute.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { userDelete } from '../../controller/users/userDelete';
+
+
+export const userDeleteRoute = Router();
+userDeleteRoute.delete('/delete/:id', userDelete);
